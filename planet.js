e32dcb0ebb864ef526eb9d8b2ccab85f5217ff80
supersixaxis <appert.nathan@outlook.fr>
2023-10-26 16:00:38 +0200

    show planet details optimisation request api

--- planet.js
+++ planet.js
@@ -10,36 +10,69 @@ const terrain = document.querySelector('.infos_terrain');
 const population = document.querySelector('.population');
 const namePlanet = document.querySelector(".planetName");
 
-let planetsData = [];
+let allPlanets = []; 
 
-async function getData(url) {
-    const response = await fetch(url);
+async function getData(url, page = 1) {
+    const response = await fetch(`${url}?page=${page}`);
     const data = await response.json();
     return data;
 }
 
-async function fetchAndDisplayPlanets() {
+
+
+
+async function fetchAllPlanets() {
     try {
-        const data = await getData('https://swapi.dev/api/planets');
-        if (Array.isArray(data.results)) {
-            planetsData = data.results;
-            planetsData.forEach(planet => {
-                const planetListItem = document.createElement('li');
-                planetListItem.classList.add('planet');
-                planetListItem.innerHTML = `<p>${planet.name}</p><p>${planet.terrain}</p>`;
-                planetList.appendChild(planetListItem);
-            });
-        } else {
-            console.error('La structure de la réponse de l\'API n\'est pas conforme aux attentes.');
+        let nextPage = "https://swapi.dev/api/planets/";
+
+        while (nextPage) {
+            const response = await fetch(nextPage);
+            const data = await response.json();
+
+            if (Array.isArray(data.results)) {
+                allPlanets = allPlanets.concat(data.results);
+            } else {
+                console.error('La structure de la réponse de l\'API n\'est pas conforme aux attentes.');
+                break;
+            }
+
+            nextPage = data.next;
         }
     } catch (error) {
-        console.error('Une erreur s\'est produite :', error);
+        console.error('Une erreur s\'est produite lors de la récupération des planètes :', error);
+        throw error;
     }
 }
 
+function displayPlanetsInHTML() {
+    try {
+        allPlanets.forEach(planet => {
+            const planetListItem = document.createElement('li');
+            planetListItem.classList.add('planet');
+            planetListItem.innerHTML = `<p>${planet.name}</p><p>${planet.terrain}</p>`;
+            planetList.appendChild(planetListItem);
+        });
+    } catch (error) {
+        console.error('Une erreur s\'est produite lors de l\'affichage des planètes dans le HTML :', error);
+    }
+}
+
+async function fetchAndDisplayPlanets() {
+    try {
+        await fetchAllPlanets();
+        displayPlanetsInHTML();
+    } catch (error) {
+        console.error('Une erreur s\'est produite lors du traitement des planètes :', error);
+    }
+}
+
+
+
+
+
 async function getCountPage() {
-    const countPlanets = planetsData.length;
-    console.log(planetsData.length)
+    const countPlanets = allPlanets.length;
+    console.log(allPlanets.length)
     planetResult.textContent = countPlanets + " resultat(s)";
 }
 
@@ -47,7 +80,7 @@ async function showPlanetInfos(event) {
     const planetNameElement = event.target.firstElementChild;
     if (planetNameElement) {
         const planetName = planetNameElement.textContent;
-        const foundPlanet = planetsData.find(planet => planet.name === planetName);
+        const foundPlanet = allPlanets.find(planet => planet.name === planetName);
         if (foundPlanet) {
             const planetDiameter = foundPlanet.diameter;
             const planetClimate = foundPlanet.climate;
